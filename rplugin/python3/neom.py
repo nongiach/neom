2288eedc9532b3c4cd7482c1ccf4b6fb4e7d6d46
chaignc <chaignc@>
2021-02-08 17:28:26 +0100

    title level: done

--- rplugin/python3/neom.py
+++ rplugin/python3/neom.py
@@ -1,24 +1,48 @@
 import neovim
 
+# https://github.com/neovim/pynvim
+
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    def title_level(line):
+    def title_level(self, line):
         i = 0
-        while line[i] == "#":
+        while i < len(line) and line[i] == "#":
             i += 1
         return i
 
+    def find_previous_title_level(self):
+        cur_row, cur_col = self.vim.current.window.cursor
+        for row in range(cur_row - 2, -1, -1):
+            level = self.title_level(self.vim.current.buffer[row])
+            if level > 0:
+                return level
+        return 1
+
+    @property 
+    def buffer(self):
+        return self.vim.current.buffer
+
+    @property
+    def current_line(self):
+        row, col = self.vim.current.window.cursor
+        # (row is 1-based, col is 0-based) 
+        return self.buffer[row-1]
+
+    @current_line.setter
+    def current_line(self, new_line):
+        row, col = self.vim.current.window.cursor
+        self.buffer[row-1] = new_line
+
     @neovim.function('UpdateCurrentTitleLevel')
     def doItPython(self, args):
-        row, col = self.vim.current.window.cursor
-        buffer = self.vim.current.buffer
-        if len(buffer[row-1]) == 1:
-            i = 0
-            for row in range(len(buffer)):
-                if buffer[row].startswith('#'):
-                    i+= 0
-            self.vim.command(f'echo "i = {i}"')
+        if len(self.current_line) == 1 and self.current_line.endswith('#'):
+            level = self.find_previous_title_level()
+            self.current_line = '#' * level
+            self.vim.command(f'echo "level = {level}"')
+            self.vim.command('startinsert!')
+        else:
+            self.vim.feedkeys('a')
 
