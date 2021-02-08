f358077bf4fd8f70d66b32a06f92f19e8efe805b
chaignc <chaignc@>
2021-02-08 16:22:12 +0100

    rename func

--- plugin/neom.vim
+++ plugin/neom.vim
@@ -5,6 +5,24 @@
 
 echo "Starting the example Python Plugin"
 
+if !has("python3")
+    echo "vim has to be compiled with +python3 to run this"
+    finish
+endif
+
+if exists('g:neom_plugin_loaded')
+    finish
+endif
+
 function DoItVimL()
     echo "hello from DoItVimL"
 endfunction
+
+" inoremap # #<esc>:exec DoItPython()
+"
+inoremap # #<esc>:call DoItPythonn()<CR>
+
+
+" the rest of plugin VimL code goes here
+
+let g:neom_plugin_loaded = 1
--- rplugin/python3/neom.py
+++ rplugin/python3/neom.py
@@ -5,7 +5,20 @@ class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.function('DoItPython')
+    def title_level(line):
+        i = 0
+        while line[i] == "#":
+            i += 1
+        return i
+
+    @neovim.function('DoItPythonn')
     def doItPython(self, args):
-        self.vim.command('echo "hello from DoItPython"')
+        row, col = self.vim.current.window.cursor
+        buffer = self.vim.current.buffer
+        if len(buffer[row-1]) == 1:
+            i = 0
+            for row in range(len(buffer)):
+                if buffer[row].startswith('#'):
+                    i+= 0
+            self.vim.command(f'echo "i = {i}"')
 
